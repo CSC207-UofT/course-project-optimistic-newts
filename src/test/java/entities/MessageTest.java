99b6b115fa132dab8bf01ea9e65e9dd88a85efb3
wilgagne <william.gagne@mail.utoronto.ca>
2021-10-08 15:27:41 -0400

    I'm not 100% sure the writtenAt and LastUpdateAt will behave as expected. I am gonna keep researching this, and I will make appropriate changes according to my findings. Insights are welcome.

---
+++ src/test/java/entities/MessageTest.java
@@ -0,0 +1,46 @@
+package entities;
+
+import org.junit.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.Assert.*;
+
+ /**
+  * A class that the test the Message class
+  */
+ public class MessageTest {
+     Message m;
+
+     @Before
+     public void setUp() throws Exception {
+         m = new Message("Luke, I am your father", "Darth Vader");
+     }
+
+     @Test(timeout = 50)
+     public void TestGetBody() {
+         assertEquals("Luke, I am your father", m.getBody());
+     }
+
+     @Test(timeout = 50)
+     public void TestGetAuthor() {
+         assertEquals("Darth Vader", m.getAuthor());
+     }
+
+     @Test(timeout = 50)
+     public void TestGetWrittenAT() {
+         DateTimeFormatter date = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+         String testDate = LocalDateTime.now().format(date);
+         assertEquals(testDate, m.getWrittenAt());
+     }
+
+     @Test(timeout = 50)
+     public void TestGetLastUpdatedAt_and_TestSetBody() {
+         m.setBody("May the force be with you");
+         DateTimeFormatter date = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+         String testDate = LocalDateTime.now().format(date);
+         assertEquals(testDate, m.getLastUpdatedAt());
+         assertEquals("May the force be with you", m.getBody());
+     }
+ }
