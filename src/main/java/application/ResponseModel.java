7d3295ce96d386254c553135de6783b7f92e5bac
Spencer Murray <spencer.murray@mail.utoronto.ca>
2021-10-09 15:26:29 -0400

    Added OutputBoundary interface and ResponseModel class

---
+++ src/main/java/application/ResponseModel.java
@@ -0,0 +1,25 @@
+package application;
+
+/**
+ * A data structure representing a response passed through an OutputBoundary Interface.
+ *
+ */
+public class ResponseModel {
+    private final String data;
+
+    /**
+     * Construct a new ResponseModel with provided data.
+     * @param data  response data
+     */
+    public ResponseModel(String data) {
+        this.data = data;
+    }
+
+    /**
+     * Return this ResponseModel's data.
+     * @return  response data
+     */
+    public String getData() {
+        return this.data;
+    }
+}
