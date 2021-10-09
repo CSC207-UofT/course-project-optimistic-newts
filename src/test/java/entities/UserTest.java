19ebf2ff9f92b1d88997aaa9203da040e5467881
Yukthiw <59380056+Yukthiw@users.noreply.github.com>
2021-10-09 18:11:52 -0400

    Implemented tests for getInterests, addInterests and removeInterest in UserTest. Also added jdoc to UserTest class.

--- src/test/java/entities/UserTest.java
+++ src/test/java/entities/UserTest.java
@@ -12,6 +12,10 @@ import static org.junit.Assert.assertTrue;
 
 public class UserTest {
     User u;
+
+    /**
+     * Setting up a test User
+     */
     @Before
     public void setUp() {
         ArrayList<String> interests = new ArrayList<String>();
@@ -20,40 +24,69 @@ public class UserTest {
         interests.add("Fitness");
         u = new User("test_user", "password123", interests, 1);
     }
+
+    /**
+     * To be implemented
+     */
     @Test
     public void isLoggedIn() {
         assertTrue(false);
     }
 
+    /**
+     * Tests the getID method
+     */
     @Test
     public void testGetId() {
         assertEquals(u.getId(), 1);
     }
 
+    /**
+     * Tests getUsername method
+     */
     @Test
     public void testGetUsername() {assertEquals(u.getUsername(), "test_user");}
 
+    /**
+     * Tests getPassword method
+     */
     @Test
     public void testGetPassword() {assertEquals(u.getPassword(), "password123");}
 
+    /**
+     * To be implemented
+     */
     @Test
     public void testGetLocation() {
         assertTrue(false);
     }
 
+    /**
+     * Tests getInterests method
+     */
     @Test
     public void testGetInterests() {
-        assertTrue(false);
+        assertEquals(u.getInterests().get(0), "Sports");
+        assertEquals(u.getInterests().get(1), "Music");
+        assertEquals(u.getInterests().get(2), "Fitness");
     }
 
+    /**
+     * Tests addInterests method
+     */
     @Test
     public void addInterests() {
-        assertTrue(false);
+        u.addInterests("Stocks");
+        assertEquals(u.getInterests().get(3), "Stocks");
     }
 
+    /**
+     * Tests removeInterest method
+     */
     @Test
     public void removeInterest() {
-        assertTrue(false);
+        u.addInterests("Stocks");
+        assertEquals(u.getInterests().get(2), "Fitness");
     }
 
 
