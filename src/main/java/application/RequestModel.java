4c9faa58b9cee51ac6567a24098d11191911603e
Spencer Murray <spencer.murray@mail.utoronto.ca>
2021-10-10 10:46:35 -0400

    Changed Response/Request data array to Map

--- src/main/java/application/RequestModel.java
+++ src/main/java/application/RequestModel.java
@@ -1,27 +1,29 @@
 package application;
 
+import java.util.Map;
+
 /**
  * A data structure representing a request passed through an InputBoundary Interface.
  */
 public class RequestModel {
-    private final Object[] data;
+    private final Map<String, Object> data;
     private final OutputBoundary respondTo;
 
     /**
-     * Construct a new RequestModel with an output location and provided data array.
+     * Construct a new RequestModel with an output location and provided data map (String keys, Object values).
      * @param respondTo     OutputBoundary to send response by
-     * @param data          Request data array
+     * @param data          Request data map
      */
-    public RequestModel(OutputBoundary respondTo, Object[] data) {
+    public RequestModel(OutputBoundary respondTo, Map<String, Object> data) {
         this.respondTo = respondTo;
         this.data = data;
     }
 
     /**
      * Return this RequestModel's data.
-     * @return  Request data
+     * @return  Request data map
      */
-    public Object[] getData() {
+    public Map<String, Object> getData() {
         return this.data;
     }
 
--- src/main/java/application/ResponseModel.java
+++ src/main/java/application/ResponseModel.java
@@ -1,24 +1,26 @@
 package application;
 
+import java.util.Map;
+
 /**
  * A data structure representing a response passed through an OutputBoundary Interface.
  */
 public class ResponseModel {
-    private final Object[] data;
+    private final Map<String, Object> data;
 
     /**
-     * Construct a new ResponseModel with provided data array.
-     * @param data  response data array
+     * Construct a new ResponseModel with provided data map (string keys, object values).
+     * @param data  Response data map
      */
-    public ResponseModel(Object[] data) {
+    public ResponseModel(Map<String, Object> data) {
         this.data = data;
     }
 
     /**
-     * Return this ResponseModel's data array.
-     * @return  response data array
+     * Return this ResponseModel's data map.
+     * @return  Response data array
      */
-    public Object[] getData() {
+    public Map<String, Object> getData() {
         return this.data;
     }
 }
