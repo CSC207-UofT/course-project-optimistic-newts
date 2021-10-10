22fcb7d95f77a608b2e693f4885b5bbac16061f2
Spencer Murray <spencer.murray@mail.utoronto.ca>
2021-10-10 15:26:57 -0400

    Modified RequestModel to be abstract, having subclasses implement concrete Request format. Also removed abstract contructor requiring a parameter from UserInteractor and ConversationInteractor.

--- src/main/java/application/ConversationInteractor.java
+++ src/main/java/application/ConversationInteractor.java
@@ -6,22 +6,10 @@ import entities.Conversation;
  * Requires simply that an implementing class stores a Conversation object and accepts requests.
  */
 public abstract class ConversationInteractor implements InputBoundary{
-    private final Conversation conversation;
+    private Conversation conversation;
 
     /**
      * Construct a new ConversationInteractor object to interact with a Conversation.
-     * @param conversation  Conversation to interact with
      */
-    public ConversationInteractor(Conversation conversation) {
-        this.conversation = conversation;
-    };
-
-    /**
-     * Accept a given (RequestModel) request.
-     * @param request   a request stored as a RequestModel
-     */
-    @Override
-    public void request(RequestModel request) {
-
-    }
+    public ConversationInteractor() {}
 }
--- src/main/java/application/UserInteractor.java
+++ src/main/java/application/UserInteractor.java
@@ -6,22 +6,10 @@ import entities.User;
  * Requires simply that an implementing class stores a User object and accepts requests.
  */
 public abstract class UserInteractor implements InputBoundary{
-    private final User user;
+    private User user;
 
     /**
      * Construct a new UserInteractor object to interact with a User.
-     * @param user  User to interact with
      */
-    public UserInteractor(User user) {
-        this.user = user;
-    }
-
-    /**
-     * Accept a given request (RequestModel).
-     * @param request   a request stored as a RequestModel
-     */
-    @Override
-    public void request(RequestModel request) {
-
-    }
+    public UserInteractor() {}
 }
