6534570803e6a493d6d6e8e92d39eb2591343e41
Taehwan Park <taehwan.park@mail.utoronto.ca>
2021-10-11 14:53:22 -0400

    Changed data type of CQ's interests from String[] to ArrayList<String> in ConversationQueue and KeyedConversation / Implemented GetRelevantConversations and GetRelevantConversations.Test / Implemented DataBase. I made DataBase since I needed a collection of all conversations for CQ.addAll() in GetRelevantConversations

---
+++ src/main/java/entities/KeyedConversation.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A KeyedConversation. Used to pair a conversation with an integer key representing the relevance to
+ * a ConversationQueue's ordering.
+ */
+public class KeyedConversation {
+    private Conversation conversation;
+    private int key = 0;
+
+    /**
+     * Create a new KeyedConversation, where key is the number of topic matches.
+     * @param conversation  Conversation to pair key to
+     * @param params        Relevance parameters
+     */
+    public KeyedConversation(Conversation conversation, ArrayList<String> params) {
+        this.conversation = conversation;
+        key = 0;
+        for (String param : params) {
+            if (conversation.getTopic().equals(param)) {
+                key += 1;
+            }
+        }
+    }
+
+    /**
+     * @return  The conversation associated with this KeyedConversation.
+     */
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    /**
+     * @return  The key associated with this KeyedConversation.
+     */
+    public int getKey() {
+        return key;
+    }
+
+    /**
+     * Primarily intended for testing purposes.
+     * @param key   key to set this KeyedConversation's key to.
+     */
+    public void setKey(int key) {this.key = key;}
+
+    /**
+     * @param other an instance of KeyedConversation
+     * @return true iff the contained conversations in this and other are equal
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof KeyedConversation)) {
+            return false;
+        }
+        return (this.conversation == ((KeyedConversation) other).getConversation());
+    }
+}
