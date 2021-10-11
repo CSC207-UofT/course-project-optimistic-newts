7ef56b3554fc1a87276054157673b24e3ee35760
Spencer Murray <spencer.murray@mail.utoronto.ca>
2021-10-11 01:17:21 -0400

    Changed ConversationQueue to deal with conversations externally, rather than KeyedConversations.

--- src/main/java/entities/KeyedConversation.java
+++ src/main/java/entities/KeyedConversation.java
@@ -26,6 +26,13 @@ public class KeyedConversation {
     }
 
     /**
+     * @return  The conversation associated with this KeyedConversation.
+     */
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    /**
      * @return  The key associated with this KeyedConversation.
      */
     public int getKey() {
@@ -37,4 +44,16 @@ public class KeyedConversation {
      * @param key   key to set this KeyedConversation's key to.
      */
     public void setKey(int key) {this.key = key;}
+
+    /**
+     * @param other an instance of KeyedConversation
+     * @return true iff the contained conversations in this and other are equal
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof KeyedConversation)) {
+            return false;
+        }
+        return (this.conversation == ((KeyedConversation) other).getConversation());
+    }
 }
