ccf00b7c13e4130403fb38ba0b8a8b96342d05fe
Taehwan Park <taehwan.park@mail.utoronto.ca>
2021-10-25 13:18:00 -0400

    Refactored the private variables in UserManager into the method in UserManager. Added the docs for the parameters in the methods of UserManager.

--- src/main/java/application/UserManager.java
+++ src/main/java/application/UserManager.java
@@ -9,20 +9,19 @@ import java.util.ArrayList;
  * An object representing a UserManager of the application.
  */
 public class UserManager {
-    private CreateUser createUser;
-    private LoginUser loginUser;
-
-    public UserManager() {
-        createUser = new CreateUser();
-        loginUser = new LoginUser();
-    }
 
     /**
      * creates and returns a new user with the given arguments.
+     * @param respondTo the OutputBoundary to receive the return
+     * @param username the username of the new user
+     * @param password the password of the new user
+     * @param location the location of the new user
+     * @param interests the interests of the new user
      * @return returns the new created user
      */
     public User CreateUser(OutputBoundary respondTo, String username, String password, String location,
                            ArrayList<String> interests){
+        CreateUser createUser = new CreateUser();
         CreateUser.CreateUserRequest request = createUser.getRequestModel();
         request.fillRequest(respondTo, 1, username, password, location, interests);
         createUser.request(request);
@@ -31,10 +30,13 @@ public class UserManager {
 
     /**
      * creates and returns a new user with the given arguments.
-     * @param
+     * @param respondTo the OutputBoundary to receive the return
+     * @param username the username of the user
+     * @param password the password of the user
      * @return returns the new created user
      */
     public User LoginUser(OutputBoundary respondTo, String username, String password){
+        LoginUser loginUser = new LoginUser();
         LoginUser.LoginUserRequest request = loginUser.getRequestModel();
         request.fillRequest(respondTo, username, password);
         loginUser.request(request);
