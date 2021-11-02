7c0f2a9e10660dae7a1ed2b7e6738e28d21368f3
wilgagne <william.gagne@mail.utoronto.ca>
2021-11-02 12:10:00 -0400

    Added Javadoc to MessageInteractor.

--- src/main/java/application/messageInteractor/MessageInteractor.java
+++ src/main/java/application/messageInteractor/MessageInteractor.java
@@ -2,7 +2,10 @@ package application.messageInteractor;
 import application.InputBoundary;
 import entities.Message;
 
+/**
+ * An abstract MessageInteractor object. Generally to be extended as a specific Message usecase.
+ * Requires simply that an implementing class stores a Message object and handles requests.
+ */
 public abstract class MessageInteractor implements InputBoundary {
     private Message m;
-
 }
