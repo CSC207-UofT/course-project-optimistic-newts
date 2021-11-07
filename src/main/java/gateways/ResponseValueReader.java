89b29ff2586eb1d79aa97f50abfe3f7112f9fb1c
Zachary Lee <zachary.lee@mail.utoronto.ca>
2021-11-07 12:21:14 -0500

    Implemented the JSON reader for the response values. Also used a new library so make note of that.

---
+++ src/main/java/gateways/ResponseValueReader.java
@@ -0,0 +1,45 @@
+package gateways;
+
+import application.ConfigReader;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class ResponseValueReader implements ConfigReader {
+    private HashMap<String, String> ResponseValues = new HashMap<>();
+
+    public ResponseValueReader() throws IOException, ParseException{
+        String RESPONSE_VALUE_PATH = "configData/ResponseValues.JSON/";
+
+        JSONParser jsonparser = new JSONParser();
+
+        FileReader reader = new FileReader(RESPONSE_VALUE_PATH);
+
+        Object obj = jsonparser.parse(reader);
+
+        JSONObject jsonObject = (JSONObject)obj;
+
+        if (jsonObject.size() > 0){
+            for (Object o : jsonObject.keySet()) {
+                String key = (String) o;
+                String val = (String) jsonObject.get(key);
+                this.ResponseValues.put(key, val);
+            }
+        }
+    }
+
+    /**
+     * Return the value of the Response given key
+      * @param key The key of the string
+     * @return The response string
+     * @throws Exception if no such key exists
+     */
+    @Override
+    public String get(String key) throws Exception {
+        return this.ResponseValues.get(key);
+    }
+}
