4732ed99a6ade773a6fcb7cf76fa65873964ca59
Zachary Lee <zachary.lee@mail.utoronto.ca>
2021-11-08 13:46:26 -0500

    Reworked the Change Conversation status test to work with the new gateway.

--- src/test/java/application/ChangeConversationStatusTest.java
+++ src/test/java/application/ChangeConversationStatusTest.java
@@ -2,15 +2,19 @@ package application;
 
 import application.conversationInteractors.ChangeConversationStatus;
 import entities.Conversation;
+import gateways.ResponseValueReader;
+import org.json.simple.parser.ParseException;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class ChangeConversationStatusTest {
     ChangeConversationStatus c;
+    ResponseValueReader r;
 
     private static class TestOutput implements OutputBoundary {
 
@@ -23,21 +27,22 @@ public class ChangeConversationStatusTest {
      *  Setting up a test conversation
      */
     @Before
-    public void setUp(){
+    public void setUp() throws IOException, ParseException {
         c = new ChangeConversationStatus();
+        r = new ResponseValueReader();
     }
 
     /**
      * Tests the request method
      */
     @Test
-    public void testRequest() {
+    public void testRequest() throws Exception {
         RequestModel request = new RequestModel(new TestOutput());
         DataBase.addConversation(new Conversation("Sample Conversation", "", "", "", 0, 0, 0,
                 "", false, new ArrayList<>(), new ArrayList<>()));
         request.fill(RequestField.ID, "Sample Conversation");
 
-        c.request(request);
+        c.request(request, r);
 
         assertTrue(DataBase.getConversation("Sample Conversation").getIsOpen());
     }
