dffd7d85ebdc88d24080e6b9c2b077168dd81cbd
Taehwan Park <taehwan.park@mail.utoronto.ca>
2021-11-08 22:04:53 -0500

    <feat>: variable name convention and request() changed in AddFriend.

--- src/main/java/application/userInteractors/AddFriend.java
+++ src/main/java/application/userInteractors/AddFriend.java
@@ -3,9 +3,13 @@ package application.userInteractors;
 import application.*;
 import entities.User;
 
+/**
+ * UserInteracotor for adding a friend.
+ */
 public class AddFriend extends UserInteractor {
     private User user;
-    private User user_two;
+    private User userTwo;
+
     /**
      * Accepts a request.
      * @param request   a request stored as a RequestModel
@@ -16,15 +20,15 @@ public class AddFriend extends UserInteractor {
         ConfigReader config = (ConfigReader) request.get(RequestField.CONFIG);
 
         user = DataBase.getUser((String) request.get(RequestField.USERNAME));
-        user_two = DataBase.getUser((String) request.get(RequestField.USERNAME_TWO));
+        userTwo = DataBase.getUser((String) request.get(RequestField.USERNAME_TWO));
 
-        if (user.getUsername() == null) {
+        if (user.getUsername() == null || userTwo.getUsername() == null) {
             // Output an error because there is no such user with the given username
             response.fill(ResponseField.FAILURE, new Exception(ApplicationExceptions.NO_SUCH_USER_ERROR));
         } else {
             // A User with the given username was found
             response.fill(ResponseField.SUCCESS, user.getUsername() + config.get("friendAdded"));
-            user.addFriend(user_two);
+            user.addFriend(userTwo);
         }
         // send response through provided output boundary
         request.getOutput().respond(response);
@@ -43,6 +47,6 @@ public class AddFriend extends UserInteractor {
      * @return  the searched User or null if no user has been logged in.
      */
     public User getUserTwo() {
-        return user_two;
+        return userTwo;
     }
 }
