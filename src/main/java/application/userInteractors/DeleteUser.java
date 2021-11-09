1ede6dff03a6b5803986a75248dd11ead7dc5382
Type your name here <alex.szokolay@mail.utoronto.ca>
2021-11-08 21:24:42 -0500

    changed user Intractors to use config.get insead of ResponseModel Response Fields.

--- src/main/java/application/userInteractors/DeleteUser.java
+++ src/main/java/application/userInteractors/DeleteUser.java
@@ -9,15 +9,17 @@ public class DeleteUser extends UserInteractor {
 
 
     @Override
-    public void request(RequestModel request) {
-        String ID = (String) request.get(RequestField.ID);
+    public void request(RequestModel request) throws Exception {
         ResponseModel response = new ResponseModel();
+        ConfigReader config = (ConfigReader) request.get(RequestField.CONFIG);
+
+        String ID = (String) request.get(RequestField.ID);
 
         if (DataBase.containsUserID(ID) == Boolean.TRUE) {
             DataBase.deleteUser(ID);
-            response.fill(ResponseField.SUCCESS, ResponseValues.deletedUser);
+            response.fill(ResponseField.SUCCESS, config.get("deletedUser"));
         } else {
-            response.fill(ResponseField.FAILURE, ResponseValues.InvalidID);
+            response.fill(ResponseField.FAILURE, config.get("InvalidID"));
         }
         // send response through provided output boundary
         request.getOutput().respond(response);
