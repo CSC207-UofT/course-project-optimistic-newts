38b5910f37b781a5cfded82ecd3513749050a192
Zachary Lee <zachary.lee@mail.utoronto.ca>
2021-11-08 22:21:23 -0500

    Removed throwing exception in interface.

--- src/main/java/application/ConfigReader.java
+++ src/main/java/application/ConfigReader.java
@@ -6,7 +6,6 @@ public interface ConfigReader {
      * Return the string with the given key.
      * @param key The key of the string
      * @return The string
-     * @throws Exception If no key value pair exists with the given key
      */
-    String get(String key) throws Exception;
+    String get(String key);
 }
